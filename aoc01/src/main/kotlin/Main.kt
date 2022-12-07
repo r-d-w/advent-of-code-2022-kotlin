dfd391496e8bdae404e834aae1b24975d83533cf
Ryan Williams <rdw@cedar.com>
2022-12-06 21:45:14 -0500

    new approach

--- aoc01/src/main/kotlin/Main.kt
+++ aoc01/src/main/kotlin/Main.kt
@@ -1,19 +1,29 @@
 import java.io.File
 
+class Elf (val id: Int, var calories: Int)
+
 fun main() {
     val inputFile: String = "/Users/rdw/git/advent-of-code-2022-kotlin/aoc01/src/main/resources/input"
-    val elves: MutableMap<Int, Int> = mutableMapOf()
+    val elvesById: MutableMap<Int, Elf> = mutableMapOf()
+    val elvesByCalories: MutableMap<Int, MutableList<Elf>> = mutableMapOf()
     var elfNumber: Int = 0
-    var topElf: Int = 0
-    File(inputFile).forEachLine(action = fun(line: String) {
+    File(inputFile).forEachLine(action = fileLoop@ fun(line: String) {
+        val currentElf = elvesById.getOrElse(elfNumber) { Elf(id = elfNumber, calories = 0) }
         if (line == "") {
+            if (currentElf.calories in elvesByCalories) {
+                elvesByCalories[currentElf.calories]!!.add(currentElf)
+            } else {
+                elvesByCalories[currentElf.calories] = mutableListOf<Elf>(currentElf)
+            }
             elfNumber++
-            return@forEachLine
+            return@fileLoop
         }
-        elves[elfNumber] = elves.getOrElse(elfNumber) { 0 } + line.replace("\n", "").toInt()
-        if (elves[elfNumber]!! > elves[topElf]!!)
-            topElf = elfNumber
+        currentElf.calories = currentElf.calories + line.replace("\n", "").toInt()
+        elvesById[elfNumber] = currentElf
     })
+    val caloriesSortedAsc: List<Elf> = elvesByCalories.keys.sortedDescending().map{elvesByCalories[it]!![0]}
+    val topThree: Int = caloriesSortedAsc[0].calories + caloriesSortedAsc[1].calories + caloriesSortedAsc[2].calories
 
-    println("Top Elf: $topElf Calories: ${elves[topElf]}")
+    println("Top Elf: ${caloriesSortedAsc[0].id} Calories: ${caloriesSortedAsc[0].calories}")
+    println("Top 3 Elves: $topThree")
 }
